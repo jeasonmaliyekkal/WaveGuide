f2c0b55e51d55daa174ee0df6e991fd5fb24b44f
jeasonmaliyekkal <jeasonpaulmaliyekkal@gmail.com>
2023-04-15 19:17:32 +0100

    fixed find angle error

--- detection.cpp
+++ detection.cpp
@@ -12,6 +12,18 @@ void setCursor(int x, int y){
   XCloseDisplay(dpy);
 }
 
+double findPointsDistance(Point a, Point b) {
+    Point difference = a - b;
+    return sqrt(difference.ddot(difference));
+}
+
+double findAngle(Point a, Point b, Point c) {
+    double ab = findPointsDistance(c, b);
+    double bc = findPointsDistance(a, b);
+    double ca = findPointsDistance(a, c);
+    return acos((bc * bc + ca * ca - ab * ab) / (2 * bc * ca));
+}
+
 void click(int button, bool state){
     Display* dpy = XOpenDisplay(NULL);
     // button = left mouse button and button = 2 for middle button and button = 3 for right button.
