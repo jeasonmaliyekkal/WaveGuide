ce228a5711ebb66eee2bd841147169d17e407473
jeasonmaliyekkal <jeasonpaulmaliyekkal@gmail.com>
2023-04-16 23:00:50 +0100

    added

---
+++ tests/build/CMakeFiles/Finger_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Finger_test.dir/Finger_test.cpp.o"
+  "CMakeFiles/Finger_test.dir/home/pi/WaveGuide/src/Finger_counter.cpp.o"
+  "Finger_test"
+  "Finger_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Finger_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
